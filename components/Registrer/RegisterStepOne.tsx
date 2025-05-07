26eab061a61a62b654abce5200aa746f7898b8a8
Petko <petko.georgiev133@gmail.com>
2025-05-07 21:05:32 +0300

    Added basic first step

--- components/Registrer/RegisterStepOne.tsx
+++ components/Registrer/RegisterStepOne.tsx
@@ -1,8 +1,17 @@
 'use client';
-import { Button, Image, Text } from '@chakra-ui/react';
-import React from 'react';
+import { Button, Image, Input, Text } from '@chakra-ui/react';
+import React, { useState } from 'react';
 import BackIcon from '../../public/back-icon.svg';
 import NextImage from 'next/image';
+import { Controller, SubmitHandler, useForm } from 'react-hook-form';
+import Select, { GroupBase } from 'react-select';
+
+type FormFields = {
+    name: string;
+    password: string;
+    confirmPassword: string;
+    hobbies: { label: string; value: string }[];
+};
 
 interface Props {
     setStep: (step: string) => void;
@@ -10,6 +19,46 @@ interface Props {
 
 const RegisterStepOne = (props: Props) => {
     const { setStep } = props;
+
+    const [selectedOptions, setSelectedOptions] = useState<
+        { label: string; value: string }[]
+    >([]);
+
+    const {
+        control,
+        register,
+        handleSubmit,
+        setError,
+        formState: { errors, isSubmitting },
+    } = useForm<FormFields>();
+
+    const onSubmit: SubmitHandler<FormFields> = (data) => {
+        if (data.password.length < 4) {
+            setError('password', {
+                message: 'Password must be at least 4 characters long',
+            });
+            return;
+        }
+
+        if (data.password !== data.confirmPassword) {
+            setError('confirmPassword', {
+                message: 'Passwords do not match',
+            });
+            return;
+        }
+    };
+
+    const hobbies: GroupBase<{ label: string; value: string }>[] = [
+        {
+            label: 'Hobbies',
+            options: [
+                { label: 'React.js', value: 'react' },
+                { label: 'Vue.js', value: 'vue' },
+                { label: 'Angular', value: 'angular' },
+                { label: 'Svelte', value: 'svelte' },
+            ],
+        },
+    ];
 
     return (
         <div>
@@ -41,15 +90,93 @@ const RegisterStepOne = (props: Props) => {
                 </Button>
                 Register
             </Text>
-            <Button
-                colorPalette='teal'
-                variant='subtle'
-                size='lg'
-                width='full'
-                onClick={() => setStep('registerStepTwo')}
-            >
-                Next
-            </Button>
+
+            <form className='tutorial gap-2' onSubmit={handleSubmit(onSubmit)}>
+                <Input
+                    mb='2'
+                    {...register('name', {
+                        required: 'Please enter a username',
+                    })}
+                    type='text'
+                    placeholder='Username'
+                />
+                {errors.name && (
+                    <div className='text-red-500 text-base! mb-2!'>
+                        {errors.name.message}
+                    </div>
+                )}
+                <Input
+                    mb='2'
+                    {...register('password', {
+                        required: 'Please enter a password',
+                    })}
+                    type='password'
+                    placeholder='Password'
+                />
+                {errors.password && (
+                    <div className='text-red-500 text-base! mb-2!'>
+                        {errors.password.message}
+                    </div>
+                )}
+                <Input
+                    mb='2'
+                    {...register('confirmPassword', {
+                        required: 'Please confirm your password',
+                    })}
+                    type='password'
+                    placeholder='Confirm Password'
+                />
+                {errors.confirmPassword && (
+                    <div className='text-red-500 text-base! mb-2!'>
+                        {errors.confirmPassword.message}
+                    </div>
+                )}
+                <Controller
+                    name='hobbies'
+                    control={control}
+                    render={({ field }) => (
+                        <Select
+                            {...field}
+                            isMulti
+                            value={selectedOptions}
+                            onChange={(selected) => {
+                                setSelectedOptions(
+                                    selected as {
+                                        label: string;
+                                        value: string;
+                                    }[]
+                                );
+                                if (selected.length <= 2) {
+                                    field.onChange(selected);
+                                }
+                            }}
+                            options={hobbies}
+                            closeMenuOnSelect={false}
+                            // only allow user to choose up to 2 options
+                            isOptionDisabled={() => selectedOptions.length >= 2}
+                            className='basic-multi-select text-black mb-4!'
+                            classNamePrefix='select'
+                        />
+                    )}
+                />
+
+                <Button
+                    mb='2'
+                    type='submit'
+                    colorPalette='teal'
+                    variant='subtle'
+                    size='lg'
+                    width='full'
+                    disabled={isSubmitting}
+                >
+                    {isSubmitting ? 'Loading...' : 'Submit'}
+                </Button>
+                {errors.root && (
+                    <div className='text-red-500 text-base! mb-2!'>
+                        {errors.root.message}
+                    </div>
+                )}
+            </form>
         </div>
     );
 };
